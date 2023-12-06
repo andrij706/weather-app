fb52056a7e1854f4f2879d76e3a9dbd42810f0ab
andrij706 <andrijlw706@gmail.com>
2023-12-06 16:14:58 +0200

    add Navigation

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,10 @@ import axios from 'axios';
 import db from './firebase/firebaseInit.js'
 import { collection, onSnapshot, query, updateDoc, doc } from 'firebase/firestore'
 import { RouterView } from 'vue-router'
+import Navigation from './components/Navigation.vue'
 
 const APIkey = ref('24b4d192d17e2f7a8780dbf71a17dde0')
-const city = ref('Lviv')
+//const city = ref('Lviv')
 const cities = ref([])
 
 const getCityWeather = async () => {
@@ -15,14 +16,18 @@ const getCityWeather = async () => {
   const querySnapshot = await onSnapshot(q, (snapshot) => {
     snapshot.docChanges().forEach(async (change) =>{
       if (change.type === 'added'){
-        console.log(change.doc.data())
+        //console.log(change.doc.data())
         try{
           const responce = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${change.doc.data().city}&appid=${APIkey.value}`)
           const data = responce.data
           await updateDoc(doc(db, 'cities', change.doc.id), {
             currentWheater: data
+          }).then(() => {
+            cities.value.push(change.doc.data())
+          }).then(() => {
+            console.log(cities.value)
           })
-          console.log(data)
+          //console.log(data)
         }catch(err){
           console.log(err)
         }
@@ -34,26 +39,42 @@ const getCityWeather = async () => {
 getCityWeather()  
 
 
-const getCurrentWeather = async () =>{
+/*const getCurrentWeather = async () =>{
   await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${APIkey.value}`).then(res =>{console.log(res.data)})
 }
 
-getCurrentWeather()
+getCurrentWeather()*/
 </script>
 
 <template>
-  <h1>App</h1>
-  <RouterView />
+  <div class="main">
+    <Navigation class="navigation"/>
+    <RouterView />
+  </div>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
 
 *{
   margin: 0;
   padding: 0;
   box-sizing: border-box;
   font-family: "Quicksand", sans-serif;
-  background: black;
-  color: aliceblue;
+}
+
+.main{
+  height: 100vh;
+
+  .navigation{
+    z-index: 99;
+    position: fixed;
+    max-width: 1024px;
+    width: 100%;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  }
+
+  .container{
+    padding: 0 20px;
+  }
 }
 </style>
